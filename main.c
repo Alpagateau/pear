a062e4e6c99fd3d65ea45ce9bed8d022905b6c68
Alpa <52045495+Alpagateau@users.noreply.github.com>
2025-10-02 11:50:22 +0200

    Parsing inputs better, checking if a file is a pear file

--- main.c
+++ main.c
@@ -12,14 +12,37 @@ int main(int argc, char **argv)
     printf("  pear <files> -[v|c|o]\n");
     return 0;
   }
+ 
+  string_ll_t args = {0};
 
   //Read the compile flags
   for(int i = 1; i < argc; i++)
   {
     if(strcmp(argv[i], "-v") == 0)
+    {
       cf.verbose = true;
-    if(strcmp(argv[i], "-c") == 0)
+    }
+    else if(strcmp(argv[i], "-c") == 0)
+    {
       cf.out_c = true;
+    }
+    else 
+    {
+      ll_append(&args, argv[i]);
+    }
   }
+
+  if(cf.verbose)
+  {
+    int size = ll_size(&args);
+    char buf[STRING_SIZE] = {0};
+    printf("Read %d arguments\n", size);
+    for(int i = 0; i < size; i++)
+    {
+      ll_cpyat(&args, i, buf);
+      printf("Parsed arg %d : %s | Is a pear file ? %d\n", i, buf, is_pear_file(buf));
+    }
+  }
+
   return 0;
 }
--- pear.h
+++ pear.h
@@ -1,6 +1,8 @@
 #pragma once
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 #define VERSION "0.0.1"
 #define STRING_SIZE 255
@@ -14,6 +16,7 @@ typedef struct string_ll string_ll_t;
 
 struct string_ll{ 
   char value[STRING_SIZE];
+  bool is_written;
   string_ll_t *next;
 };
 
@@ -23,8 +26,21 @@ int ll_append(string_ll_t *root, char* val)
     ll_append(root->next, val);
   else 
   {
-    root->next = (string_ll_t*)malloc(sizeof(string_ll_t));
-    strcpy(root->next->value, val);
+    if(root->is_written != 0)
+    {
+      root->next = (string_ll_t*)malloc(sizeof(string_ll_t));
+      if(root->next == NULL)
+      {
+        return 1;
+      }
+      root->next->is_written = 1;
+      strcpy(root->next->value, val);
+    }
+    else 
+    {
+      strcpy(root->value, val);
+      root->is_written = 1; 
+    }
   }
   return 0;
 }
@@ -40,4 +56,58 @@ int ll_pop(string_ll_t *root, char* out)
     root->next = NULL;
   }
   return 0;
+}
+
+int ll_size(string_ll_t *root)
+{
+  if(root->next == NULL)
+    return 1;
+  else 
+    return 1 + ll_size(root->next);
+}
+
+int ll_cpyat(string_ll_t *root, unsigned int index, char* buffer)
+{
+  if(index == 0)
+    strcpy(buffer, root->value);
+  else 
+  {
+    if(root->next == NULL)
+      return 1;
+    else 
+      return ll_cpyat(root->next, index-1, buffer);
+  }
+  return 0;
+}
+
+char* ll_getat(string_ll_t *root, unsigned int index, int* size_out)
+{
+  if(index == 0)
+  {
+    *size_out = STRING_SIZE;
+    return root->value;
+  }
+  else 
+  {
+    if(root->next == NULL)
+    {
+      *size_out = 0;
+      return NULL;
+    }
+    else { 
+      return ll_getat(root->next, index-1, size_out);
+    }
+  }
+  return 0;
+}
+
+bool is_pear_file(char* filename){
+  int len = strlen(filename);
+  if(len < 4)
+    return false;
+  char* ext = (filename + len - 5);
+  if(strcmp(ext, ".pear") == 0)
+    return true;
+  else 
+    return false;
 }
